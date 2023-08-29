255dd5eb0a7b82dab300592f37e16f70660ca034
Rutjake <jarnosanterimusic@gmail.com>
2023-08-29 20:29:17 +0300

    Fonts And Layout

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ const api = {
   lang: "fi"
 }
 
+
 function App() {
   // UseStates
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
 
   // Fetch From Api
-  const search = evt => {
+  const search = (evt) => {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&lang=${api.lang}&units=${api.units}&appid=${api.key}`)
         .then(res => res.json())
@@ -27,6 +28,15 @@ function App() {
     }
   }
 
+  // Get Weather Icon
+  const icon = () => {
+    let get_icon = weather.weather[0].icon;
+    let icon_url = "https://openweathermap.org/img/wn/";
+    let format = ".png";
+
+  return `${icon_url}${get_icon}${format}`;
+
+}
   // Get Current Date
   const dates = (d) => {
     // Date lists
@@ -40,7 +50,7 @@ function App() {
     let year = d.getFullYear();
 
     // Return Variables
-    return `${day} ${date} ${month} ${year}`
+    return `${day} ${date} ${month} ${year}`;
   }
 
   return (
@@ -48,7 +58,7 @@ function App() {
       <div className={(typeof weather.main != "undefined") ? ((weather.main.temp > 16) ? 'app warm' : 'app') : 'app'}>
         <main>
           <div className='content_box'>
-            <h1>Säätiedot tänään</h1>
+            <h1>Sää nyt</h1>
             <div className="search_box">
               <input
                 type="text"
@@ -63,13 +73,37 @@ function App() {
               <div>
                 <div className="location-box">
                   <div className="location">{weather.name}, {weather.sys.country}</div>
-                  <div className="date">{dates(new Date())}</div>
-                </div>
-                <div className="weather-box">
-                  <div className="temp">
-                    {Math.round(weather.main.temp)}°c
+                  <div className="date">
+                    {dates(new Date())}
                   </div>
-                  <div className="weather">{weather.weather[0].description}</div>
+                </div>
+
+                <div className="weather-box">
+                  <div className="temp-cont">
+                    <div className="icon">
+                      <img src={icon()} className="icon_img" alt="Weather Icon"></img>
+                    </div>
+                    <div className="temp">
+                      {Math.round(weather.main.temp)}°c
+                    </div>
+                  </div>
+                  <div className="info-cont">
+                    <div className="desc">
+                      {weather.weather[0].description}
+                    </div>
+                    <div className="feels">
+                      Tuntuu kuin: {Math.round(weather.main.feels_like)}°c
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="humidity">
+                    Kosteus: {weather.main.humidity}%
+                  </div>
+                 
+                  <div className="wind">
+                    Tuulen nopeus: {weather.wind.speed} m/s
+                  </div>
+                  <hr />
                 </div>
               </div>
             ) : ('')}
